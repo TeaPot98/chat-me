51e4f7cd7da75d9fe8416f60ff947e20c3872bd8
Vlad Trifautan <vlad.trifautan@mail.ebs-integrator.com>
2022-10-17 15:24:19 +0300

    styled MessageField

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        // background: "rgb(230,233,241)",
+        height: "80vh",
+        background:
+          "linear-gradient(0deg, rgba(230,233,241,1) 0%, rgba(233,199,225,1) 54%, rgba(246,223,210,1) 100%)",
+      }}
+      className="App"
+    >
       <MessageField />
     </div>
   );
