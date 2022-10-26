ff3c4677430d6b08c6f68df6c1d7127e679134b3
TeaPot98 <trifautan@gmail.com>
2022-10-26 09:23:45 +0300

    added LoadingIndicator component

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,14 @@
+export const LoadingIndicator = () => {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
